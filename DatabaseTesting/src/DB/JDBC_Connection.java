cf86293adfdf1bf60bb94ff036df649457912824
Antony <antony@yopmail.com>
2024-11-28 01:33:00 +0530

    11/28/2024

--- DatabaseTesting/src/DB/JDBC_Connection.java
+++ DatabaseTesting/src/DB/JDBC_Connection.java
@@ -6,6 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JDBC_Connection {
 
 	public static void main(String[] args) throws SQLException {
@@ -20,11 +24,15 @@ public class JDBC_Connection {
 		Statement s = con.createStatement();
 		ResultSet rs = s.executeQuery("Select * from employee_details where id=3");
 //        rs.next(): Moves the cursor to the next row in the ResultSet. Returns false when no more rows are available.
-
-		while (rs.next()) {
-			System.out.println(rs.getString("Location"));
-			System.out.println(rs.getInt("age"));
-		}
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://login.salesforce.com/?locale=in");
+        rs.next();
+		//while (rs.next()) {
+			driver.findElement(By.id("username")).sendKeys(rs.getString("Location"));
+			driver.findElement(By.id("password")).sendKeys(rs.getString("Emp_name"));
+//			System.out.println(rs.getString("Location"));
+//			System.out.println(rs.getInt("age"));
+		
 
 	}
 }
--- Practice/src/com/prac/Frames_Prac.java
+++ Practice/src/com/prac/Frames_Prac.java
@@ -7,14 +7,19 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Frames_Prac {
 
+	// Frames are HTML elements (<iframe>, <frame>, or <frameset>) used to embed
+	// another HTML document within a parent document. In Selenium, you must switch
+	// to a frame before interacting with elements inside it, as frames isolate
+	// their content from the main DOM.
 	public static void main(String[] args) {
 		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/");
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.cssSelector("a[href*='nested'"))).click().build().perform();
+		a.moveToElement(driver.findElement(By.cssSelector("a[href*='nested']"))).click().build().perform();
 		System.out.println(driver.findElements(By.tagName("frameset")).size());
 		System.out.println(driver.findElements(By.tagName("frame")).size());
+		//used frame name
 		driver.switchTo().frame("frame-top");
 		driver.switchTo().frame("frame-middle");
 		String text = driver.findElement(By.xpath("//div[@id='content']")).getText();
